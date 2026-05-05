f1d7fa5a54f77d7034563ee52c478552ee5baa38
puzevich <puzevich.svetlana@gmail.com>
2026-05-05 16:14:51 +0300

    feat: add pagination

---
+++ rs-react-app/src/components/Pagination.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './search.bar.module.css';
+
+interface Props {
+    count: number;
+    currentPage: number;
+    onPageChange: (page: number) => void;
+}
+
+class Pagination extends React.Component<Props> {
+
+    handlePageChange = (event: React.MouseEvent, page: number) => {
+        event.preventDefault();
+        this.props.onPageChange(page);
+    }
+
+    render() {
+        const { count, currentPage } = this.props;
+
+        if (count <= 1) {
+            return null;
+        }
+
+        const pages = Array.from({ length: count }, (_, index) => index + 1);
+
+        return (
+            <div className={styles.pagination}>
+                {pages.map((page) => {
+                    return (
+                        <a
+                            key={page}
+                            href="#"
+                            className={page === currentPage ? styles.activePage : styles.page}
+                            onClick={(e) => this.handlePageChange(e, page)}
+                        >
+                            {page}
+                        </a>
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default Pagination;
--- rs-react-app/src/components/SearchResult.tsx
+++ rs-react-app/src/components/SearchResult.tsx
@@ -1,33 +1,72 @@
 import { Component } from 'react';
 import type { Pokemon } from '../types.ts';
 import styles from './search.bar.module.css';
+import Pagination from "./Pagination.tsx";
 
 interface Props {
   pokemons: Pokemon[];
 }
 
-class SearchResult extends Component<Props> {
+interface State {
+  currentPage: number;
+}
+
+const POKEMON_PAGE_SIZE = 10;
+
+class SearchResult extends Component<Props, State> {
+  state: State = {
+    currentPage: 1,
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (prevProps.pokemons !== this.props.pokemons) {
+      this.setState({ currentPage: 1 });
+    }
+  }
+
+  handlePageChange = (page: number) => {
+    this.setState({
+      currentPage: page
+    });
+  }
+
   render() {
-    if (this.props.pokemons.length === 0) {
+    const { pokemons } = this.props;
+    const { currentPage } = this.state;
+
+    if (pokemons.length === 0) {
       return <div>No such pokemon</div>;
     }
+
+    const pagesCount = Math.ceil(pokemons.length / POKEMON_PAGE_SIZE);
+
+    const startIndex = (currentPage - 1) * POKEMON_PAGE_SIZE;
+    const currentPokemons = pokemons.slice(startIndex, startIndex + POKEMON_PAGE_SIZE);
+
     return (
-      <div>
-        <h2>List of pokemons</h2>
-        {this.props.pokemons.map((item: Pokemon) => (
-          <div key={item.id} className={styles.item}>
-            <figure className={styles.imgContainer}>
-              <img
-                className={styles.pokemonImg}
-                src={item.image}
-                alt={item.name}
-              />
-              <figcaption>{item.name}</figcaption>
-            </figure>
-            <div>{item.abilities}</div>
+        <>
+          <div>
+            <h2>List of pokemons</h2>
+            {currentPokemons.map((item: Pokemon) => (
+                <div key={item.id} className={styles.item}>
+                  <figure className={styles.imgContainer}>
+                    <img
+                        className={styles.pokemonImg}
+                        src={item.image}
+                        alt={item.name}
+                    />
+                    <figcaption className={styles.caption}>{item.name}</figcaption>
+                  </figure>
+                  {item.abilities && <div>{item.abilities}</div>}
+                </div>
+            ))}
           </div>
-        ))}
-      </div>
+          <Pagination
+              count={pagesCount}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+          />
+        </>
     );
   }
 }
