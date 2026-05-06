2fddf0e41da7053e3b01b630af9712ba5291b1f4
puzevich <puzevich.svetlana@gmail.com>
2026-05-06 18:56:55 +0300

    fix: deploy

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import React, { type ErrorInfo, type ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          Here is test for error boundary. Instead of pokemon items you can see
+          this text. Reload page to continue
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
