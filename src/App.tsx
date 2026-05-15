7959c8fb5f4749def89596f4917f7b26ca11e22d
puzevich <puzevich.svetlana@gmail.com>
2026-05-15 11:29:34 +0300

    fix: layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout.tsx';
 import ErrorBoundary from './components/errorBoundary/ErrorBoundary.tsx';
+import MainLayout from './layouts/main/MainLayout.tsx';
 import SearchPage from './pages/searchPage/SearchPage.tsx';
 import AboutPage from './pages/about/AboutPage.tsx';
 import NotFoundPage from './pages/notFound/NotFoundPage.tsx';
+import EmptyLayout from './layouts/empty/EmptyLayout.tsx';
 
 class App extends Component {
   render() {
@@ -13,10 +14,15 @@ class App extends Component {
       <BrowserRouter>
         <MainLayout>
           <Routes>
-            <Route path="/" element={<SearchPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<SearchPage />} />
+              <Route path="/about" element={<AboutPage />} />
+            </Route>
+            <Route element={<EmptyLayout />}>
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
           </Routes>
+
         </MainLayout>
       </BrowserRouter>
       </ErrorBoundary>
