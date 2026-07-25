3bcb7569f32c99f9a860c53ecf6ecb02a8543b10
puzevich <puzevich.svetlana@gmail.com>
2026-07-25 22:42:54 +0300

    fix: base-url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokemon-search/',
+  base: '/rsschool-react/',
   plugins: [react()],
   test: {
     globals: true,
